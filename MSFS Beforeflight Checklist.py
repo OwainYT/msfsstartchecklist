68d29c4facb813e6151ba6b0df2e0e4217b725f3
Owain <88115597+OwainYT@users.noreply.github.com>
2024-04-28 02:18:54 +0100

    removing the stupid macos things
    1st update :)

--- MSFS Beforeflight Checklist.py
+++ MSFS Beforeflight Checklist.py
@@ -2,6 +2,13 @@ import tkinter as tk
 import logging
 import sys
 import os
+import subprocess
+
+# open the variables file
+with open('variables.txt', 'r') as file:
+    file_contents = file.read()
+logging.info('Opening variables.txt')
+logging.info (print(file_contents))
 
 logging.critical('Hello World!')
 # debug thing
@@ -22,19 +29,12 @@ def on_leave(event):
     button3.config(text="Exit")
 
 # actions
-def restart_program():
-    python = sys.executable
-    os.execl(python, python, *sys.argv)
-def create_ui():
-    root = tk.Tk()
-def restart_and_close():
-    restart_program()
-    root.destroy()
 def greet():
     print("Hello, Tkinter!")
     logging.warning('button pressed')
 def command1():
     print("Button 1")
+
     logging.warning('button pressed')
 def command2():
     print("Button 2")
@@ -57,19 +57,16 @@ logging.warning('window created')
 button = tk.Button(root, text="Owain's MSFS Before-Start Checklist", command=NotImplemented)
 button.grid(row=0, column=5)
 
-button1 = tk.Button(root, text="Greet", command=command1)
+button1 = tk.Button(root, text="Start Volanta", command=command1)
 button1.grid(row=1, column=0)
 
-button2 = tk.Button(root, text="Greet", command=command2)
+button2 = tk.Button(root, text="Start MSFS", command=command2)
 button2.grid(row=2, column=0)
 
 button3 = tk.Button(root, text="Exit", command=command3)
 button3.grid(row=3, column=5)
 button3.bind("<Enter>", on_enter)
 button3.bind("<Leave>", on_leave)
-
-restart_button = tk.Button(root, text="Restart", command=restart_and_close)
-restart_button.grid(row=4, column=0)
 
 logging.warning('buttons created')
 
